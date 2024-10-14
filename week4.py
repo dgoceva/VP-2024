0611333b210fefaabc81ec5724732e70811d8096
dgoceva <44052125+dgoceva@users.noreply.github.com>
2024-10-14 13:04:24 +0300

    14-10-2024-2

--- week4.py
+++ week4.py
@@ -1,5 +1,5 @@
 # # COEFF = 1.96
-COEFF =  1.955
+# COEFF =  1.955
 # euro = float(input("EUR: "))
 # print(f"BGN: {round(euro*COEFF,2)}")
 
@@ -14,7 +14,19 @@ print(tuple)
 tuple = (3,)
 print(tuple)
 
-PySimpleGUI_License = 'owggSIrkq5rhPbeWKVHMmYJX6NM0VIZjGovgOIninI7suIcCNJUDPb72S1lwwYyWV5F57IGjNoLgDIVl6BJ57UX2glbtsckG5xClaUx2M9JmGdnCPIDs0ImCOJ5DqdwX9NO09b32610lxctkflJ'
+URL = "https://api.exchangerate-api.com/v4/latest/EUR"
+
+import requests
+
+response = requests.get(URL)
+print(response.json()["rates"])
+print(response.status_code)
+if response.status_code ==200:
+    COEFF = response.json()["rates"]["BGN"]
+else:
+    COEFF =  1.955
+
+# PySimpleGUI_License = 'owggSIrkq5rhPbeWKVHMmYJX6NM0VIZjGovgOIninI7suIcCNJUDPb72S1lwwYyWV5F57IGjNoLgDIVl6BJ57UX2glbtsckG5xClaUx2M9JmGdnCPIDs0ImCOJ5DqdwX9NO09b32610lxctkflJ'
 
 import PySimpleGUI as sg
 
@@ -26,8 +38,12 @@ import PySimpleGUI as pg
 # ]
 
 layout = [
-    [pg.Text("EUR:"),pg.InputText(focus=True),pg.Button("Convert")],
-    [pg.Text("BGN:"),pg.InputText(readonly=True),pg.Button("Close")]
+    [pg.Combo(("EUR","BGN"),default_value="EUR",
+              key="--IN-MONEY--",enable_events=True,readonly=True),
+              pg.InputText(focus=True,key="--IN--"),pg.Button("Convert",bind_return_key=True)],
+    [pg.Combo(("EUR","BGN"),default_value="BGN",readonly=True,
+              key="--OUT-MONEY--"),
+              pg.InputText(readonly=True,key="--OUT--"),pg.Button("Close")]
 ]
 
 pg.theme("BluePurple")
@@ -38,11 +54,23 @@ while True:
 
     if event in (pg.WIN_CLOSED,"Close"):
         break
-    elif event=="Convert":
-        # print(values[0])
-        euro = float(values[0])
-        result = round(euro*COEFF,2)
-        # print(result)
-        window[1].update(result)
+    elif event =="--IN-MONEY--":
+        money_type = values["--IN-MONEY--"]
+        if money_type == "EUR":
+            new_money = "BGN"
+        else:
+            new_money = "EUR"
+        window["--OUT-MONEY--"].update(new_money)
+    elif event=="Convert" and values["--IN--"]!="":
+        euro = float(values["--IN--"])
+        if euro <= 0:
+            window["--IN--"].update("")
+            window["--OUT--"].update("")
+            continue
+        if values["--IN-MONEY--"]=="EUR":
+            result = round(euro*COEFF,2)
+        else:
+            result = round(euro/COEFF,2)
+        window["--OUT--"].update(result)
 
 window.close()
